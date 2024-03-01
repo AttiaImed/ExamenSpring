1915616bf0b975f4503196c839b2f12cb200c08e
ImeedAttia <attia.imeed@gmail.com>
2024-03-01 11:28:53 +0100

    second commit

---
+++ src/main/java/esprit/tn/examenspring/Aspects/LoggingAspect.java
@@ -0,0 +1,12 @@
+package esprit.tn.examenspring.Aspects;
+
+//@Component
+//@Aspect
+//@Slf4j
+//public class LoggingAspect {
+//    @Before("execution(* esprit.tn.testccattiaimed.Services.*(..))")
+//    public void logMethodEntry(JoinPoint joinPoint) {
+//        String name = joinPoint.getSignature().getName();
+//        log.info("In method " + name + " : ");
+//    }
+//}
